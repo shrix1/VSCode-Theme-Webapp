8e026ed3cc97cd0e5f2214cbaf1bf0d52344bfc2
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-26 22:37:37 +0530

    tailwind custom

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,31 +1,49 @@
 import React from "react";
-import { FaSquareFull } from "react-icons/fa";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 export default function Nav() {
   return (
     <>
       <nav className="w-[100%] p-5 bg-mainbl-200 font-pop">
-        <div>
-          <h1 className="text-3xl text-white">Theme</h1>
-        </div>
-        {/* <button>
-          <FaSquareFull />
-        </button> */}
-        <ul className="text-1xl">
-          <li>
-            <a href="/">Reviews</a>
-          </li>
-          .
-          <li>
-            <a href="/">Contact</a>
-          </li>
-          .
-          <li>
-            <a href="/">Support</a>
-          </li>
-        </ul>
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-3xl text-white">Theme</h1>
+          </div>
 
-        <button className="p-2 bg-[]">Download</button>
+          <button className="text-3xl text-white hover:text-mainpp-200">
+            <HiMenuAlt3 />
+          </button>
+        </div>
+
+        <div className="flex flex-col">
+          <ul className="text-1xl mt-2 flex justify-around relative">
+            <li>
+              <a href="/" className="text-mainbl-100 hover:text-white">
+                Reviews
+              </a>
+            </li>
+            <span className="text-mainbl-100">.</span>
+            <li>
+              <a href="/" className="text-mainbl-100 hover:text-white">
+                Contact
+              </a>
+            </li>
+            <span className="text-mainbl-100">.</span>
+            <li>
+              <a href="/" className="text-mainbl-100 hover:text-white">
+                Support
+              </a>
+            </li>
+          </ul>
+
+          <button
+            className="p-2 bg-mainpp-200 rounded-md mt-3 w-[140px] ml-[170px]
+        border-mainpp-200 hover:bg-transparent border-2 hover:border-mainpp-200
+        hover:text-white transition-colors duration-300"
+          >
+            <a href="/">Download</a>
+          </button>
+        </div>
       </nav>
     </>
   );
