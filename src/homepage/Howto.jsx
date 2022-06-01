9e9ad0eda2653909f28babfa07797eb95e22fbdc
Shriprasanna B <shriprasanna32@gmail.com>
2022-06-01 20:44:40 +0530

    c

--- src/homepage/Howto.jsx
+++ src/homepage/Howto.jsx
@@ -1,22 +1,22 @@
 const Howto = () => {
   return (
     <>
-      {/* main section */}
+      {/*--------------------------main section-------------------------- */}
       <main
         className="flex flex-col  h-[43vh] justify-evenly p-4 items-center 
         font-pop capitalize bg-mainbl-200"
       >
-        {/* title */}
+        {/*--------------------------- title---------------------------- */}
         <h1
           className="text-3xl md:text-6xl text-white font-medium
         "
         >
           how to <span className="text-mainpp-200">download</span> :
         </h1>
-        {/* main div */}
+        {/*--------------------------- main div--------------------------- */}
         <div className="md:text-3xl ">
           <div className="md:flex md:justify-between md:items-center">
-            {/* vscode dload div */}
+            {/*------------- vscode dload div----------------------- */}
             <div className="md:w-[500px] md:mr-5">
               <h1 className="text-xl md:text-3xl text-mainpp-200 tracking-[0.7px] md:mb-1">
                 From VSCODE code editor :
@@ -35,9 +35,9 @@ const Howto = () => {
               </h2>
             </div>
             <br />
-            {/* or */}
+            {/*------------------------- or------------------------------------- */}
             <h1 className="text-center text-mainpp-200">(or)</h1>
-            {/* manual dload div */}
+            {/*--------------------- manual dload div--------------------------------*/}
             <div className="md:w-[500px] md:ml-10">
               <h1 className="text-xl  md:text-3xl text-mainpp-200  tracking-[1px] md:mb-1">
                 Manual download :
